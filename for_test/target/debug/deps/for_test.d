814aa9ca31bb953a83e524412b28456a70a021d0
halacrcrc <2253453812q@gmail.com>
2022-05-21 18:03:18 +0800

    just_for_test

---
+++ for_test/target/debug/deps/for_test.d
@@ -0,0 +1,5 @@
+D:\Rust_OS\for_test\target\debug\deps\for_test.exe: src\main.rs
+
+D:\Rust_OS\for_test\target\debug\deps\for_test.d: src\main.rs
+
+src\main.rs:
---
+++ for_test/target/debug/for_test.d
@@ -0,0 +1,1 @@
+D:\Rust_OS\for_test\target\debug\for_test.exe: D:\Rust_OS\for_test\src\main.rs
